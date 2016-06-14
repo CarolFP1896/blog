63920dd81db4c3d071217832f81d88b341e5e5f4
CarolFP1896 <carolineferreirap@gmail.com>
2016-06-14 13:50:14 -0300

    mostrando erros

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,17 +9,20 @@ class ArticlesController < ApplicationController
 	end
 
 	def new
-
+		@article = Article.new
 	end
 
 	def create
-		@article = Article.new(article_params)
-		@article.save
-		redirect_to articles_path
+		@article = Article.new article_params
+		if @article.save
+			redirect_to articles_path
+		else
+			render 'new'
+		end
 	end
 
 	private
 		def article_params
 			params.require(:article).permit(:title, :text)
-		end
+		end	
 end
